db9e5dcda9d1ee5ae186b676e1fa194d7f013b8f
Koray Beyaz <koray.beyaz73@gmail.com>
2025-05-19 14:39:05 +0300

    fix tests for linear trend

--- tests/test_linear_trend.py
+++ tests/test_linear_trend.py
@@ -1,13 +1,13 @@
 from usopp import LinearTrend
+from usopp.utils import IdentityScaler
 import numpy as np
 
 
 def test_can_fit_generated_data(trend_data):
     data, true_delta, n_changepoints = trend_data
     model = LinearTrend(n_changepoints=n_changepoints)
-    model.fit(data[['t']], data["value"])
-    y_scale_factor = model._y_scaler_.std_
-    model_delta = np.mean(model.trace_[model._param_name("delta")], axis=0) * y_scale_factor
+    model.fit(data[['t']], data["value"], y_scaler=IdentityScaler)
+    model_delta = np.mean(model.trace_[model._param_name("delta")], axis=0)
     res = model.predict(data[['t']])
     np.testing.assert_allclose(model_delta, true_delta, atol=0.01)
     np.testing.assert_allclose(res.yhat.squeeze(), data.value, atol=0.01)
